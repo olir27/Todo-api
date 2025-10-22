b75d384923db3841a3787eb394956586eba64458
olir27 <jolir27@gmail.com.com>
2025-10-22 11:00:54 +0530

    backend

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 import express from 'express'
 import mongoose from 'mongoose'
+import cors from 'cors'
+
+
+
+
 const app = express();
+
+app.use(cors({
+    
+}));
 app.use(express.json());
 const todoSchema = new mongoose.Schema({
     title: { type: String, required: true },
